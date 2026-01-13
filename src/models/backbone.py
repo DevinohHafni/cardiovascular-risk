ad95c80149efbea884eb1448bc6adf91690609e9
Devinoh Hafni G <83331259+DevinohHafni@users.noreply.github.com>
2026-01-13 11:02:24 +0530

    Add ResNet18 backbone model function

--- src/models/backbone.py
+++ src/models/backbone.py
@@ -1,1 +1,9 @@
+import torch.nn as nn
+from torchvision import models
+
+def get_backbone():
+    model = models.resnet18(weights="IMAGENET1K_V1")
+    features = model.fc.in_features
+    model.fc = nn.Identity()
+    return model, features
 
