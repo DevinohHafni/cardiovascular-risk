4ab8136bf641a06e2e9b06dc6588c2e161b17bd0
Devinoh Hafni G <83331259+DevinohHafni@users.noreply.github.com>
2026-01-13 11:02:50 +0530

    Add MultiTaskHead class for multi-task learning

--- src/models/multitask_head.py
+++ src/models/multitask_head.py
@@ -1,1 +1,13 @@
+import torch.nn as nn
+
+class MultiTaskHead(nn.Module):
+    def __init__(self, in_features):
+        super().__init__()
+        self.color_head = nn.Linear(in_features, 2)   # normal, bluish
+        self.morph_head = nn.Linear(in_features, 3)  # clubbing, pitting, deformation
+
+    def forward(self, x):
+        color_logits = self.color_head(x)
+        morph_logits = self.morph_head(x)
+        return color_logits, morph_logits
 
